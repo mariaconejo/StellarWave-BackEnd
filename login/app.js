3d215a3bfa6472fa08b0459ec450f59ea17577be
Jeareth Venegas <venegasjea@gmail.com>
2021-07-30 17:14:39 -0600

    se añaden los mensajes de autenticación de usuario y contraseña

--- login/app.js
+++ login/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express;
 const encrypt = require('bcrypt');
+const user = require('./public/user');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -24,6 +25,48 @@ mongoose.connect(mongoURL, function(error){
 });
 
 
-app.length('/', (req, res) => {
+// app.length('/', (req, res) => {
 
+// });
+
+app.post('/register', (req, res) => {
+    const{name, password} = req.body;
+    const user = new user({name, password});
+    user.save( error => {
+        //validación de agregar el usuario
+        if (error){
+            res.status(500).send('¡Error al registrar al usuario!')
+        }else{
+            res.status(200).send('¡El usuario se registró con éxito!')
+        }
+    })
 });
+
+app.pos('/authenticate', (req, res) => {
+    const {name, password} = req.body;
+    // findOne nos pide un parametro para buscar un callback
+    user.findOne({name}, (error, user) => {
+        if(error){
+            //mensajes de autenticación
+            res.status(500).send('Error de autenticación del usuario');
+        }else if (!user){
+            res.status(500).send('Usuario inexistente');
+        }else{
+            user.rightPassword(password, (error, result) =>{
+                if(error){
+                    res.status(500).send('Error de autenticación');
+                }else if(result){
+                    res.status(200).send('Usuario autenticado con éxito');
+                }else{
+                    res.status(500).send('Usuario o contraseña incorrectos');
+                }
+            })
+        }
+    }) 
+});
+
+app.listen(3000, () => {
+    console.log('Server started')
+});
+
+module.exports = app;
